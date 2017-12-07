28bd19b7a73eab5d33564d18584402bb8d26ca2b
AngelRisso1337z <angel.risso@unitec.edu>
2017-12-07 12:13:16 -0600

    commit inicial

---
+++ Laboratorio_7_AngelRisso/src/laboratorio_7_angelrisso/Laboratorio_7_AngelRisso.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio_7_angelrisso;
+
+/**
+ *
+ * @author HP_AMD_64
+ */
+public class Laboratorio_7_AngelRisso {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola:v");
+        
+        
+        
+    }
+    
+}
